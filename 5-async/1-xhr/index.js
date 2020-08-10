5a350a6965d3c27f94093ede17b83c1554484a62
Yun Chenxi <cleoxiii@163.com>
2020-08-10 23:54:00 +0800

    [yunchenxi] feat: 5-async - finish xhr.

--- 5-async/1-xhr/index.js
+++ 5-async/1-xhr/index.js
@@ -2,7 +2,17 @@ function fetchData(url, successCallback, errorCallback) {
   const xhr = new XMLHttpRequest();
   // <-- start
   // TODO 21: 通过XMLHttpRequest实现异步请求
-
+  xhr.open('POST', url, true);
+  xhr.setRequestHeader('content-type', 'application/json');
+  // eslint-disable-next-line func-names
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status >= 200 && xhr.status <= 299) {
+      successCallback(xhr);
+    }
+    if (xhr.readyState === 4 && (xhr.status < 200 || xhr.status > 299)) {
+      errorCallback(xhr);
+    }
+  };
   // end -->
 }
 
