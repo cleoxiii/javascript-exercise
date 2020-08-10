cdb8637db6dd47b0a6de999c3697c3a378dc8e23
Yun Chenxi <cleoxiii@163.com>
2020-08-10 22:20:24 +0800

    [yunchenxi] feat: 3-object - finish assign.

--- 3-object/assign/index.js
+++ 3-object/assign/index.js
@@ -1,3 +1,6 @@
 export default function addSerialNumber(source) {
   // TODO 5: 在这里写实现代码，需要采用ES6 Object.assign
+  // eslint-disable-next-line no-param-reassign
+  source.properties.status = 'processed';
+  return Object.assign({ serialNumber: '12345' }, source);
 }
