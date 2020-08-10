f5145d9e0cb72b5c83c07cc32c87ad8c6a6cf453
Yun Chenxi <cleoxiii@163.com>
2020-08-10 22:35:08 +0800

    [yunchenxi] feat: 3-object - finish values.

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,6 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  return Object.values(source)
+    .map(i => parseInt(i, 10))
+    .reduce((cur, pre) => cur + pre, 0);
 }
