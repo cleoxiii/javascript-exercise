d2d19f192a2309f531b170e115b1e35f91a0793d
Yun Chenxi <cleoxiii@163.com>
2020-08-10 21:46:38 +0800

    [yunchenxi] feat: 2-array - finish filter.

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,11 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  return collection.filter(i => i % 3 === 0);
 }
 
 function chooseNoRepeatNumber(collection) {
   // TODO 2: 在这里写实现代码
+  return collection.filter((v, i) => collection.indexOf(v) === i);
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
